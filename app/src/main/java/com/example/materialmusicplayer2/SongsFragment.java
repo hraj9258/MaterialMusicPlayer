476b8fdd2c25178af38d6fe0d53e65cc3628ecd6
Himanshu Raj <himanshurajkush@gmail.com>
2023-10-02 22:21:40 +0530

    SongsFragment:   Switch: from String[] items to ArrayList<String> artistsName   Add:    ArrayList<String> songsName and ArrayList<String> genresNames    getArtistNamesFromMP3Files() for artistsName   Rename: ViewPager2SongsAdapter to SongsAdapterViewPager2   Clean: onCreateView() and move all the processing to onViewCreated()   Update: filterSongs() to work with  ArrayList<String>

--- app/src/main/java/com/example/materialmusicplayer2/SongsFragment.java
+++ app/src/main/java/com/example/materialmusicplayer2/SongsFragment.java
@@ -1,13 +1,16 @@
 package com.example.materialmusicplayer2;
 
+import static com.example.materialmusicplayer2.MainActivity.mySongs;
+
 import android.content.Intent;
+import android.media.MediaMetadataRetriever;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
 
-import android.os.Environment;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -18,17 +21,22 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.example.materialmusicplayer2.adapters.SongsAdapterViewPager2;
 import com.google.android.material.search.SearchView;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SongsFragment extends Fragment {
+    static ArrayList<String> artistsName;
+    static ArrayList<String> songsName = new ArrayList<>();
+    static ArrayList<String> genresNames;
     TabLayout tabLayout;
-    ViewPager2SongsAdapter viewPager2SongsAdapter;
+    SongsAdapterViewPager2 songsAdapterViewPager2;
     ViewPager2 viewPager2Songs;
 
     public SongsFragment() {
@@ -38,7 +46,18 @@ public class SongsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_songs, container, false);// Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_songs, container, false);// Inflate the layout for this fragment
+    }
+
+    /**
+     * @param view               The View returned by {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     *                           from a previous saved state as given here.
+     */
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
         tabLayout = view.findViewById(R.id.tabLayout);
         viewPager2Songs = view.findViewById(R.id.viewPager2Songs);
 
@@ -46,33 +65,31 @@ public class SongsFragment extends Fragment {
         SearchView searchView = view.findViewById(R.id.searchViewSongs);
         ListView scrollViewSuggestions = view.findViewById(R.id.scrollViewSuggestions);
 
-
         // hack: extracting the textview from the search view( please change it to native implementation of search view in future)
         EditText searchText = searchView.getEditText();
 
-        // alphabetically sorted arraylist of all songs
-        ArrayList<File> mySongs = MainActivity.fetchSongs(Environment.getExternalStorageDirectory());
-        String[] items = new String[mySongs.size()]; // string array containing names of all songs
+        // ArrayList<String> of all the tracks in mySongs
         for (int i = 0;i < mySongs.size();i++){
-            items[i] = mySongs.get(i).getName().replace(".mp3", "");
+            songsName.add(i,mySongs.get(i).getName());
         }
+
+        // ArrayList<String> of all the artist in mySongs
+        artistsName = getArtistNamesFromMP3Files(mySongs);
+
         searchText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
             }
-
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                filterSongs(s.toString(), scrollViewSuggestions, items);
+                filterSongs(s.toString(), scrollViewSuggestions, songsName);
             }
-
             @Override
             public void afterTextChanged(Editable s) {
 
             }
         });
-
         // click listeners for the listview results
         scrollViewSuggestions.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -88,14 +105,13 @@ public class SongsFragment extends Fragment {
         });
 
         // tab layout and view pager2
-        viewPager2SongsAdapter = new ViewPager2SongsAdapter(getChildFragmentManager(), getLifecycle());
-        viewPager2SongsAdapter.addFragment(new TracksTabLayoutFragment());
-        viewPager2SongsAdapter.addFragment(new ArtistsTabLayoutFragment());
-        viewPager2SongsAdapter.addFragment(new GenresTabLayoutFragment());
+        songsAdapterViewPager2 = new SongsAdapterViewPager2(getChildFragmentManager(), getLifecycle());
+        songsAdapterViewPager2.addFragment(new TracksTabLayoutFragment());
+        songsAdapterViewPager2.addFragment(new ArtistsTabLayoutFragment());
+        songsAdapterViewPager2.addFragment(new GenresTabLayoutFragment());
 
         viewPager2Songs.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
-        viewPager2Songs.setAdapter(viewPager2SongsAdapter);
-
+        viewPager2Songs.setAdapter(songsAdapterViewPager2);
 
         new TabLayoutMediator(tabLayout, viewPager2Songs, new TabLayoutMediator.TabConfigurationStrategy() {
             @Override
@@ -110,10 +126,9 @@ public class SongsFragment extends Fragment {
                 }
             }
         }).attach();
-        return view;
     }
 
-    private void filterSongs(String query, ListView listView, String[] allSongs) {
+    private void filterSongs(String query, ListView listView, ArrayList<String> allSongs) {
         List<String> filteredSongs = new ArrayList<>();
 
         for (String song : allSongs) {
@@ -125,5 +140,33 @@ public class SongsFragment extends Fragment {
         // Update the ListView with the filtered results
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), R.layout.simple_list, filteredSongs);
         listView.setAdapter(adapter);
+    }
+
+    /**
+     * @param mp3Files ArrayList<File> containing all the mp3 files
+     * @return ArrayList<String> containing all the Artists Name
+     */
+    public ArrayList<String> getArtistNamesFromMP3Files(@NonNull ArrayList<File> mp3Files) {
+        ArrayList<String> artistNames = new ArrayList<>();
+
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+
+        for (File mp3File : mp3Files) {
+            try {
+                retriever.setDataSource(mp3File.getAbsolutePath());
+                String artist = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
+                artistNames.add(artist != null ? artist : "Unknown Artist");
+            } catch (Exception e) {
+                // Handle exceptions or errors when processing the file
+                e.printStackTrace();
+                artistNames.add("Unknown Artist");
+            }
+        }
+        try {
+            retriever.release();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return artistNames;
     }
 }
