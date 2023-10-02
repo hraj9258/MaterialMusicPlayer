7e9b5c0dd5d425c4a6848e663259cd7f141f802b
Himanshu Raj <himanshurajkush@gmail.com>
2023-10-02 22:26:26 +0530

    TracksTabLayoutFragment:   Switch: From ListView to RecyclerView   Clean:    Remove some unused functions and overrides    onCreateView() and move all the processing to onViewCreated() TracksRecyclerViewAdapter: add the RecyclerView.Adapter and ViewHolder

--- app/src/main/java/com/example/materialmusicplayer2/TracksTabLayoutFragment.java
+++ app/src/main/java/com/example/materialmusicplayer2/TracksTabLayoutFragment.java
@@ -1,95 +1,44 @@
 package com.example.materialmusicplayer2;
 
-import android.content.Intent;
+import static com.example.materialmusicplayer2.SongsFragment.artistsName;
+import static com.example.materialmusicplayer2.SongsFragment.songsName;
+
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
-import java.io.File;
-import java.util.ArrayList;
+import com.example.materialmusicplayer2.adapters.TracksRecyclerViewAdapter;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TracksTabLayoutFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class TracksTabLayoutFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    TracksRecyclerViewAdapter tracksRecyclerViewAdapter;
+    RecyclerView tracksRecyclerView;
     public TracksTabLayoutFragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TracksTabLayoutFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TracksTabLayoutFragment newInstance(String param1, String param2) {
-        TracksTabLayoutFragment fragment = new TracksTabLayoutFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =inflater.inflate(R.layout.fragment_tracks_tab_layout, container, false);
-        ListView tracksListView = view.findViewById(R.id.tracksListView);
+        return inflater.inflate(R.layout.fragment_tracks_tab_layout, container, false);
+    }
 
-        ArrayList<File> mySongs = MainActivity.fetchSongs(Environment.getExternalStorageDirectory());
-        String[] items = new String[mySongs.size()];
-        for (int i = 0;i < mySongs.size();i++){
-            items[i] = mySongs.get(i).getName().replace(".mp3", "");
-        }
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), R.layout.simple_list, items);
-        tracksListView.setAdapter(adapter);
-        tracksListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(getContext(), PlaySongActivity.class);
-                String currentSong = tracksListView.getItemAtPosition(position).toString();
-                intent.putExtra("SongList", mySongs);
-                intent.putExtra("CurrentSong", currentSong);
-                intent.putExtra("Position", position);
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-                startActivity(intent);
-            }
-        });
-
-        return view;
+        // Tracks RecyclerView
+        tracksRecyclerView = view.findViewById(R.id.tracksRecyclerView);
+        tracksRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        // Tracks RecyclerView Adapter
+        tracksRecyclerViewAdapter = new TracksRecyclerViewAdapter(songsName,artistsName,getContext());
+        tracksRecyclerView.setAdapter(tracksRecyclerViewAdapter);
     }
 }
---
+++ app/src/main/java/com/example/materialmusicplayer2/adapters/TracksRecyclerViewAdapter.java
@@ -0,0 +1,91 @@
+package com.example.materialmusicplayer2.adapters;
+
+import static com.example.materialmusicplayer2.MainActivity.mySongs;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.materialmusicplayer2.PlaySongActivity;
+import com.example.materialmusicplayer2.R;
+
+import java.util.ArrayList;
+
+public class TracksRecyclerViewAdapter extends RecyclerView.Adapter<TracksRecyclerViewAdapter.ViewHolder> {
+    ArrayList<String> songsName;
+    ArrayList<String> artistsName;
+    Context context;
+
+    /**
+     * @param songsName ArrayList<String> containing all the Songs
+     * @param artistsName ArrayList<String>
+     * @param context Context
+     */
+    public TracksRecyclerViewAdapter(ArrayList<String> songsName,ArrayList<String> artistsName,Context context) {
+        this.songsName = songsName;
+        this.artistsName = artistsName;
+        this.context = context;
+    }
+
+
+    @NonNull
+    @Override
+    public TracksRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.songs_row_recycler_view,parent,false);
+        return new ViewHolder(view);
+    }
+
+    /**
+     * @param holder   The ViewHolder which should be updated to represent the contents of the
+     *                 item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.songName.setText(songsName.get(holder.getAdapterPosition()));
+        holder.artistName.setText(artistsName.get(holder.getAdapterPosition()));
+        holder.albumArt.setImageResource(R.drawable.twotone_music_note_24);
+
+        // Starting the PlaySongActivity
+        holder.songsRow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, PlaySongActivity.class);
+                String currentSong = songsName.get(holder.getAdapterPosition());
+                intent.putExtra("SongList", mySongs);
+                intent.putExtra("CurrentSong", currentSong);
+                intent.putExtra("Position", holder.getAdapterPosition());
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    /**
+     * @return no of items to show in RecyclerView
+     */
+    @Override
+    public int getItemCount() {
+        return songsName.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView albumArt;
+        TextView songName, artistName;
+        ConstraintLayout songsRow;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            albumArt = itemView.findViewById(R.id.albumArtRecyclerViewRow);
+            songName = itemView.findViewById(R.id.songNameRecyclerViewRow);
+            artistName = itemView.findViewById(R.id.artistNameRecyclerViewRow);
+            songsRow = itemView.findViewById(R.id.songsRow);
+        }
+    }
+}
