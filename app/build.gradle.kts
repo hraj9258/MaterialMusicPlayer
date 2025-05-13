316896a3630ab61bd1b306b09182fa073d423b0e
Himanshu Raj <himanshurajkush@gmail.com>
2025-05-13 20:08:42 +0530

    Update Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,10 +45,10 @@ android {
 
 dependencies {
     // Kotlin + JetPack Compose
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-    implementation("androidx.activity:activity-compose:1.9.3")
-    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
+    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
+    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation(platform("androidx.compose:compose-bom:2025.05.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -56,21 +56,21 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.12.01"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2025.05.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.0")
 
     // Material Extended Icon
-    implementation("androidx.compose.material:material-icons-extended-android:1.7.6")
+    implementation("androidx.compose.material:material-icons-extended-android:1.7.8")
 
     // Media 3
-    implementation ("androidx.media3:media3-common:1.5.1")
-    implementation ("androidx.media3:media3-exoplayer:1.5.1")
-    implementation ("androidx.media3:media3-ui:1.5.1")
-    implementation ("androidx.media3:media3-session:1.5.1")
+    implementation ("androidx.media3:media3-common:1.6.1")
+    implementation ("androidx.media3:media3-exoplayer:1.6.1")
+    implementation ("androidx.media3:media3-ui:1.6.1")
+    implementation ("androidx.media3:media3-session:1.6.1")
 
     // Permission Management replace asap
     implementation("com.karumi:dexter:6.2.3")
@@ -84,7 +84,7 @@ dependencies {
     implementation("androidx.compose.material3.adaptive:adaptive-navigation")
 
     // Koin
-    implementation(platform("io.insert-koin:koin-bom:4.1.0-Beta1"))
+    implementation(platform("io.insert-koin:koin-bom:4.1.0-Beta7"))
     implementation("io.insert-koin:koin-core")
     implementation("io.insert-koin:koin-androidx-compose")
 }
