56813abec83e423893d0453c8590e0a2de39dc36
Himanshu Raj <himanshurajkush@gmail.com>
2025-05-16 17:52:30 +0530

    add albumArt in Music data class

--- app/src/main/java/com/hraj9258/musicplayer/music/domain/Music.kt
+++ app/src/main/java/com/hraj9258/musicplayer/music/domain/Music.kt
@@ -1,7 +1,10 @@
 package com.hraj9258.musicplayer.music.domain
 
+import android.graphics.Bitmap
+
 data class Music(
     val album: String,
+    val albumArt: Bitmap? = null,
     val artist: String,
     val duration: Long,
     val id: Long,
